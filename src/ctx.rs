ca82d9c718bcc3a32d083441b5b999c6e5992e8f
Matthew <matthew@beersonthewall.com>
2024-12-27 20:36:17 -0600

    state dir

--- src/ctx.rs
+++ src/ctx.rs
@@ -1,17 +1,17 @@
 use std::path::PathBuf;
 
-const BASE_DIR: &'static str = "/var/run/generic_brand_container_runtime";
+const BASE_DIR: &'static str = "/run/generic_brand_container_runtime";
 
 /// Container runtime settings
 pub struct Ctx {
-    root_dir: PathBuf,
+    state_dir: PathBuf,
     cgroups_root: String,
 }
 
 impl Default for Ctx {
     fn default() -> Self {
         Self {
-            root_dir: PathBuf::from(BASE_DIR),
+            state_dir: PathBuf::from(BASE_DIR),
             cgroups_root: String::from("/sys/fs/cgroup"),
         }
     }
